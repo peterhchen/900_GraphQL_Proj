66f782ea29904e312c510315dd3e7dca306851d7
peterhchen <peter.hp.chen@gmail.com>
2020-09-03 19:49:23 -0700

    Add files via upload

---
+++ 04_type-data/app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bodyParser = require('body-parser'); 
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema, DangerousChangeType } = require ('graphql');
+
+const app = express();
+const events = [];
+
+app.use(bodyParser.json()); // parse json body
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: buildSchema(`
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
+            type RootQuery {
+                events: [Event!]!
+            }
+            type RootMutation {
+                createEvent(eventInput: EventInput): Event
+            }
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            events: () => {
+                return events;
+                // return ['Cooking', 'Sailing', 'Coding']
+            },
+            createEvent: (args) => {
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    // date: new Date().toString()
+                    date: args.eventInput.date
+                }
+                console.log ('args:', args);
+                console.log ('event:', event);
+                events.push(event);
+                return event;
+                // const eventName = args.name;
+                // return eventName;
+            } 
+        },
+        graphiql: true      
+    })
+);
+
+app.get('/', (req, res, next) => {
+    res.send('Hello World!');
+})
+
+app.listen(3000); // listen at port 3000. You can use different port
